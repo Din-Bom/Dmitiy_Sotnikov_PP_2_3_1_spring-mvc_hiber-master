28310b9ff97f1739b013e094fe81b5f3909fbaf3
Дмитрий Сотников <DIMA10299@mail.ru>
2024-06-04 00:13:56 +0300

    Выполнил!

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,11 +1,15 @@
 package web.model;
 
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 
 @Entity
 @Table(name = "users")
@@ -15,23 +19,27 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "name", nullable = false)
-    private final String name;
+    @Column(name = "name")
+    @Size(min = 2, max = 30, message = "The name must not be less than 2 or more than 30 characters")
+    @NotEmpty(message = "The field should not be empty")
+    private String name;
 
-    public User(String name) {
-        this.name = name;
-    }
-
-    protected User() {
-        this.name = null;
+    public User() {
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 }
